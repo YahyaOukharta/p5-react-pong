e54adfdd62bf86b5b2423d297bcbff505f425e38
Yahya Oukharta <youkhart@e1r6p1.1337.ma>
2022-05-22 05:13:55 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Pong from "./components/Pong"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Pong 
+        width={1000} height={700}
+        initBallX={500} initBallY={350} ballRadius={50} ballSpeed={10} 
+        paddleWidth={30} paddleHeight={150} paddleSpeed={10} 
+        />
     </div>
   );
 }
